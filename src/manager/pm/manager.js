c42522be1c75a061ee80230563479ae1ae3858c7
Kilian Schuster <0.duraaa@gmail.com>
2021-03-24 20:05:01 +0100

    Add invite link and confirmation handling

--- src/manager/pm/manager.js
+++ src/manager/pm/manager.js
@@ -1,5 +1,6 @@
 const Questionnaire = require('../questionnaire');
 const Bot = require('../../bot/bot');
+const Bancho = require('bancho.js');
 
 /**
  * @class Manager - Manages incoming private messages and stars a bot after gathering all data.
@@ -20,34 +21,54 @@ class Manager {
         console.log('connected');
 
         this.client.on('PM', (message) => {
-            if (message.user.username === 'BanchoBot' || message.self || message.user.isClient()) {
-                console.log('Blocked message.');
+            console.log("PRIVATE MESSAGE");
+            if (message.user.ircUsername === 'BanchoBot' || message.self || message.user.isClient()) {
+                console.log(`Blocked private message => ${message.user.ircUsername}: ${message.message}`);
                 return;
             }
 
-            const runningQuestionnaire = this.runningQuestionnaires.find((o) => o.name === message.user.username);
-            if (runningQuestionnaire) {
-                runningQuestionnaire.questionnaire.handleInput(message.message);
-                return;
-            }
-
-            const runningGame = this.runningGames.find((o) => o.name === message.user.username);
-            if (runningGame) {
-                message.user.sendMessage('You already have a game running.');
-                return;
-            }
-
-            const r = /^!new$/i;
-            if (r.test(message.message)) {
-                const questionnaire = new Questionnaire(this, message);
-                this.runningQuestionnaires.push({
-                    name: message.user.username,
-                    questionnaire,
-                });
-
-                questionnaire.ask();
-            }
+            this.handleQuestionnaire(message);
         });
+
+        // let channel = await this.client.getChannel(`#mp_78934011`);
+        // await channel.join();
+
+        // console.log("joined channel")
+
+        // this.client.on('CM', (message) => {
+        //     if (message.user.ircUsername === 'BanchoBot' || message.self) {
+        //         console.log(`Blocked channel message => ${message.user.ircUsername}: ${message.message}`);
+        //         return;
+        //     }
+
+        //     this.handleQuestionnaire(message);
+        // })
+    }
+
+    async handleQuestionnaire(message) {
+        const runningQuestionnaire = this.runningQuestionnaires.find((o) => o.name === message.user.username);
+        if (runningQuestionnaire) {
+            runningQuestionnaire.questionnaire.handleInput(message.message);
+            return;
+        }
+
+        const runningGame = this.runningGames.find((o) => o.name === message.user.username);
+        if (runningGame) {
+            message.user.sendMessage('You already have a game running.');
+            return;
+        }
+
+        const r = /^!new$/i;
+        if (r.test(message.message)) {
+            const questionnaire = new Questionnaire(this, message);
+            this.runningQuestionnaires.push({
+                name: message.user.username,
+                questionnaire,
+            });
+
+            questionnaire.ask();
+        }
+
     }
 
     /**
@@ -59,13 +80,25 @@ class Manager {
         this.runningQuestionnaires = this.runningQuestionnaires.filter((running) => questionnaire.message.user.username !== running.name);
         const { answers } = questionnaire;
 
+        if (answers.confirmation === '!cancel') {
+            questionnaire.message.user.sendMessage("Match creation cancelled.");
+            return;
+        }
+
         (() => {
             // Add bot instance to running games
             // Set creator as ref on joining
             // Enable admin commands for refs
             // Send invite link to creator
 
-            const bot = new Bot(this.client, answers.lobbyName, 0, answers.size, ['Freemod'], answers.minStars, answers.maxStars);
+            // Use new client to not have the listeners of this client
+            const Client = new Bancho.BanchoClient({
+                username: process.env.OSU_USER,
+                password: process.env.OSU_PASS,
+                apiKey: process.env.API_KEY
+            });
+
+            const bot = new Bot(Client, answers.lobbyName, 0, answers.size, ['Freemod'], answers.minStars, answers.maxStars);
             bot.lobbyListenersCallback = () => {
                 bot.channel.lobby.on('playerJoined', (playerObj) => {
                     if (playerObj.player.user.username === questionnaire.message.user.username) {
@@ -82,15 +115,14 @@ class Manager {
             });
 
             // since we're not caring about the promises resolving, we wait till the lobby is created with a timeout
-            const inviteLinkTimeout = () => {
-                if (bot.channel) {
-                    const gameId = Number(bot.channel.topic.substring(18));
-                    this.message.user.sendMessage(`Join your game here: [osump://${gameId}/ ${bot.lobbyName}]`);
+            let inviteLinkInterval = setInterval(() => {
+                if (bot.channel !== null && bot.channel.hasOwnProperty('joined') && bot.channel.joined) {
+                    const gameId = Number(bot.channel.topic.split('#')[1]);
+                    questionnaire.message.user.sendMessage(`Join your game here: [osump://${gameId}/ ${bot.lobbyName}]`);
 
-                    clearTimeout(inviteLinkTimeout);
+                    clearInterval(inviteLinkInterval);
                 }
-            };
-            setTimeout(inviteLinkTimeout, 1000);
+            }, 1000);
         })();
     }
 }
