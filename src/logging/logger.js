7a7912fbf994a554fa2a16d9de46f98aa4c7aea4
Kilian Schuster <0.duraaa@gmail.com>
2021-03-25 21:55:05 +0100

    Add winston as logger

---
+++ src/logging/logger.js
@@ -0,0 +1,11 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.cli(),
+    transports: [
+        new winston.transports.Console(),
+    ],
+});
+
+module.exports = logger;
