030c2dd0e1e9f4c342c0a5b520f921cc704d9b91
Kilian Schuster <0.duraaa@gmail.com>
2021-04-16 03:10:55 +0200

    chore: switched to maps, introduce creator param

--- src/manager/pm/manager.js
+++ src/manager/pm/manager.js
@@ -9,32 +9,36 @@ const logger = require('../../logging/logger');
 class Manager {
   /**
    *
-   * @param {BanchoClient} Client
+   * @param {Bancho.BanchoClient} Client
    */
   constructor(Client) {
     this.client = Client;
-    this.runningQuestionnaires = [];
-    this.runningGames = [];
+    this.runningQuestionnaires = new Map();
+    this.runningGames = new Map();
   }
 
   async start() {
-    await this.client.connect();
-    logger.info('connected');
+    try {
+      await this.client.connect();
+      logger.info('connected');
 
-    this.client.on('PM', (message) => {
-      if (
-        message.user.ircUsername === 'BanchoBot' ||
-        message.self ||
-        message.user.isClient()
-      ) {
-        logger.info(
-          `Blocked private message => ${message.user.ircUsername}: ${message.message}`
-        );
-        return;
-      }
+      this.client.on('PM', (message) => {
+        if (
+          message.user.ircUsername === 'BanchoBot' ||
+          message.self ||
+          message.user.isClient()
+        ) {
+          logger.info(
+            `Blocked private message => ${message.user.ircUsername}: ${message.message}`
+          );
+          return;
+        }
 
-      this.handleQuestionnaire(message);
-    });
+        this.handleQuestionnaire(message);
+      });
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   /**
@@ -42,27 +46,24 @@ class Manager {
    * @param {BanchoMessage} message
    * @returns
    */
-  async handleQuestionnaire(message) {
-    const running = this.runningQuestionnaires.find(
-      (o) => o.name === message.user.username
-    );
-    if (running) {
-      running.questionnaire.handleInput(message.message);
+  handleQuestionnaire(message) {
+    const messageUsername = message.user.ircUsername;
+    if (this.runningQuestionnaires.has(messageUsername)) {
+      this.runningQuestionnaires
+        .get(messageUsername)
+        .questionnaire.handleInput(message.message);
       return;
     }
 
     const r = /^!new$/i;
     if (r.test(message.message)) {
-      if (this.runningGames.find((o) => o.name === message.user.username)) {
+      if (this.runningGames.has(messageUsername)) {
         message.user.sendMessage('You already have a game running.');
         return;
       }
 
       const questionnaire = new Questionnaire(this, message);
-      this.runningQuestionnaires.push({
-        name: message.user.username,
-        questionnaire,
-      });
+      this.runningQuestionnaires.set(messageUsername, { questionnaire });
 
       questionnaire.ask();
     }
@@ -73,25 +74,19 @@ class Manager {
    * @param {Questionnaire} questionnaire
    */
   evaluateQuestionnaire(questionnaire) {
-    // Remove questionnaire from running questionnaires array
-    // eslint-disable-next-line
-    this.runningQuestionnaires = this.runningQuestionnaires.filter(
-      (running) => questionnaire.message.user.username !== running.name
-    );
+    const creator = questionnaire.message.user;
+
+    // Remove questionnaire from running questionnaires map
+    this.runningQuestionnaires.delete(creator.ircUsername);
 
     const { answers } = questionnaire;
 
     if (answers.confirmation === '!cancel') {
-      questionnaire.message.user.sendMessage('Match creation cancelled.');
+      creator.sendMessage('Match creation cancelled.');
       return;
     }
 
     (() => {
-      // Add bot instance to running games
-      // Set creator as ref on joining
-      // Enable admin commands for refs
-      // Send invite link to creator
-
       // Use new client to not have the listeners of this client
       const Client = new Bancho.BanchoClient({
         username: process.env.OSU_USER,
@@ -102,61 +97,22 @@ class Manager {
       const defaultOptions = {
         teamMode: 0,
         mods: ['Freemod'],
+        creator: creator,
       };
       Object.assign(answers, defaultOptions);
 
       const bot = new Bot(Client, answers);
-      bot.lobbyListenersCallback = () => {
-        const addCreatorAsRefEvent = (playerObj) => {
-          let username = questionnaire.message.user.ircUsername;
-          if (questionnaire.message.user.username !== undefined) {
-            username = questionnaire.message.user.username;
-          }
-
-          if (playerObj.player.user.username === username) {
-            bot.channel.lobby.addRef(playerObj.player.user.username);
-            bot.channel.lobby.removeListener(
-              'playerJoined',
-              addCreatorAsRefEvent
-            );
-          }
-        };
-
-        bot.channel.lobby.on('playerJoined', addCreatorAsRefEvent);
-      };
+      bot.start();
 
       bot.on('started', () => {
         bot.channel.on('PART', (member) => {
-          // The lobby has been disbanded
-          if (member.user.isClient()) {
-            bot.stop();
-            this.runningGames = this.runningGames.filter(
-              (o) => o.username === member.user.ircUsername
-            );
-          }
+          if (member.user.isClient)
+            this.runningGames.delete(member.user.ircUsername);
         });
       });
 
-      bot.start();
-
-      this.runningGames.push({
-        username: questionnaire.message.user.username,
-        bot,
-      });
-
-      // since we're not caring about the promises resolving,
-      // we wait till the lobby is created with a timeout
-      const inviteLinkInterval = setInterval(() => {
-        if (
-          bot.channel !== null &&
-          Object.prototype.hasOwnProperty.call(bot, 'joined') &&
-          bot.channel.joined
-        ) {
-          questionnaire.message.user.sendMessage(bot.getGameInviteLink());
-
-          clearInterval(inviteLinkInterval);
-        }
-      }, 1000);
+      // Add bot instance to running games
+      this.runningGames.set(creator.ircUsername, { bot });
     })();
   }
 }
